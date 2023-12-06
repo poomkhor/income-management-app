45e6490f3fdb761d2fe1b74658874e06f4a4fc2d
Poom Khorchitmate <poom.khor@gmail.com>
2023-12-06 19:05:17 +0700

    add routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
